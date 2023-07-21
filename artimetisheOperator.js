dfaff48f90fe343adb164a8d4b9b9c59e3305062
DenizMv <denizmehmedov573@gmail.com>
2023-07-21 23:48:13 +0200

    Update artimetisheOperator.js
    firststepintojs

--- artimetisheOperator.js
+++ artimetisheOperator.js
@@ -1,2 +1,19 @@
-console.cleart()
-let name
+console.clear();
+console.log("Hallo");
+
+let name = 'Deniz'
+let age = "14";
+let months = age * 12;
+let days = age * 365;
+let hours = days * 24;
+let minutes = hours * 60;
+let seconds = minutes * 60;
+
+console.log("Mein Name ist " + name + ".");
+console.log("bla bal bla");
+console.log("Ich bin " + age + " Jahre alt");
+console.log("Ich bin " + months + " Monate alt")
+console.log("Ich bin " + days + "Tage alt")
+console.log("Ich bin " + hours +" Stunden alt")
+console.log("Ich bin " + minutes + " Minuten alt")
+console.log("ich bin " + seconds +" Sekunden alt")
